66efda1c848ae52607acebed730b1a18d261d235
ersinev <ersinevci@hotmail.com>
2022-05-30 09:05:46 +0200

    updated

---
+++ javascript/starting/operations.js
@@ -0,0 +1,15 @@
+// Math operations
+
+console.log(10+4)
+console.log('48'*5);
+let user = 'Ersin2'
+let x =console.log(user.includes('1'))
+// function correct(){
+    
+//     if (x==false){
+//         document.write('you can log in');
+//     }
+    
+// }
+
+
