c3835d81d5746f22d05b6c9540f404fdb7447859
ersinev <ersinevci@hotmail.com>
2022-06-17 17:50:22 +0200

    updated

--- zzHouse/house.js
+++ zzHouse/house.js
@@ -1,144 +1,252 @@
 const houses = [
+  {
+    id: 1,
+    pand: "huis",
+    prijs: 350.0,
+    oppervlak: 200,
+    stad: "Brussel",
+    kamers: 2,
+    tuin: true,
+  },
 
-    {id: 11,
-    
+  {
+    id: 2,
     pand: "appartement",
-    
-    prijs: 310.000,
-    
-    oppervlak: 200,
-    
-    stad: "Brussel",
-    
-    kamers: 3,
-    
-    tuin: false,
-},
-{id: 12,
-    
-    pand: "house",
-    
-    prijs: 250.000,
-    
-    oppervlak: 150,
-    
-    stad: "Antwerpen",
-    
-    kamers: 2,
-    
-    tuin: true,
-},
-{id: 13,
-    
-    pand: "house",
-    
-    prijs: 500.000,
-    
-    oppervlak: 400,
-    
-    stad: "Brussel",
-    
-    kamers: 5,
-    
-    tuin: true,
-},
-{id: 14,
-    
-    pand: "appartement",
-    
-    prijs: 100.000,
-    
+    prijs: 150.0,
     oppervlak: 100,
-    
-    stad: "Brussel",
-    
-    kamers: 2,
-    
+    stad: "Gent",
+    kamers: 1,
     tuin: false,
-},
-{id: 15,
-    
+  },
+
+  {
+    id: 3,
+    pand: "huis",
+    prijs: 700.0,
+    oppervlak: 500,
+    stad: "Antwerpen",
+    kamers: 4,
+    tuin: true,
+  },
+
+  {
+    id: 4,
     pand: "appartement",
-    
-    prijs: 369.000,
-    
+    prijs: 300.0,
+    oppervlak: 150,
+    stad: "Gent",
+    kamers: 3,
+    tuin: false,
+  },
+
+  {
+    id: 5,
+    pand: "huis",
+    prijs: 500.0,
+    oppervlak: 350,
+    stad: "Brussel",
+    kamers: 4,
+    tuin: false,
+  },
+
+  {
+    id: 6,
+    pand: "appartement",
+    prijs: 250.0,
+    oppervlak: 100,
+    stad: "Brussel",
+    kamers: 1,
+    tuin: true,
+  },
+
+  {
+    id: 7,
+    pand: "huis",
+    prijs: 375.0,
+    oppervlak: 300,
+    stad: "Antwerpen",
+    kamers: 2,
+    tuin: true,
+  },
+
+  {
+    id: 8,
+    pand: "appartement",
+    prijs: 400.0,
     oppervlak: 250,
-    
-    stad: "Brussel",
-    
+    stad: "Antwerpen",
     kamers: 3,
-    
-    tuin: true,
-},
-{id: 16,
-    
-    pand: "house",
-    
-    prijs: 370.000,
-    
-    oppervlak: 225,
-    
-    stad: "Brussel",
-    
-    kamers: 3,
-    
-    tuin: true,
-},
-{id: 17,
-    
-    pand: "house",
-    
-    prijs: 150.000,
-    
-    oppervlak: 100,
-    
-    stad: "Gent",
-    
-    kamers: 2,
-    
     tuin: false,
-},
-{id: 18,
-    
+  },
+
+  {
+    id: 9,
+    pand: "huis",
+    prijs: 150.0,
+    oppervlak: 100,
+    stad: "Brussel",
+    kamers: 1,
+    tuin: false,
+  },
+
+  {
+    id: 10,
     pand: "appartement",
-    
-    prijs: 200.000,
-    
-    oppervlak: 150,
-    
+    prijs: 330.0,
+    oppervlak: 240,
     stad: "Gent",
-    
     kamers: 2,
-    
     tuin: true,
-},
-{id: 19,
-    
+  },
+
+  {
+    id: 11,
+
     pand: "appartement",
-    
-    prijs: 350.000,
-    
+
+    prijs: 310.0,
+
     oppervlak: 200,
-    
-    stad: "Antwerpen",
-    
-    kamers: 2,
-    
-    tuin: true,
-},
-{id: 20,
-    
-    pand: "house",
-    
-    prijs: 380.000,
-    
-    oppervlak: 310,
-    
-    stad: "Antwerpen",
-    
+
+    stad: "Brussel",
+
     kamers: 3,
-    
+
+    tuin: false,
+  },
+  {
+    id: 12,
+
+    pand: "house",
+
+    prijs: 250.0,
+
+    oppervlak: 150,
+
+    stad: "Antwerpen",
+
+    kamers: 2,
+
     tuin: true,
-}
-    
-    ]
+  },
+  {
+    id: 13,
+
+    pand: "house",
+
+    prijs: 500.0,
+
+    oppervlak: 400,
+
+    stad: "Brussel",
+
+    kamers: 5,
+
+    tuin: true,
+  },
+  {
+    id: 14,
+
+    pand: "appartement",
+
+    prijs: 100.0,
+
+    oppervlak: 100,
+
+    stad: "Brussel",
+
+    kamers: 2,
+
+    tuin: false,
+  },
+  {
+    id: 15,
+
+    pand: "appartement",
+
+    prijs: 369.0,
+
+    oppervlak: 250,
+
+    stad: "Brussel",
+
+    kamers: 3,
+
+    tuin: true,
+  },
+  {
+    id: 16,
+
+    pand: "house",
+
+    prijs: 370.0,
+
+    oppervlak: 225,
+
+    stad: "Brussel",
+
+    kamers: 3,
+
+    tuin: true,
+  },
+  {
+    id: 17,
+
+    pand: "house",
+
+    prijs: 150.0,
+
+    oppervlak: 100,
+
+    stad: "Gent",
+
+    kamers: 2,
+
+    tuin: false,
+  },
+  {
+    id: 18,
+
+    pand: "appartement",
+
+    prijs: 200.0,
+
+    oppervlak: 150,
+
+    stad: "Gent",
+
+    kamers: 2,
+
+    tuin: true,
+  },
+  {
+    id: 19,
+
+    pand: "appartement",
+
+    prijs: 350.0,
+
+    oppervlak: 200,
+
+    stad: "Antwerpen",
+
+    kamers: 2,
+
+    tuin: true,
+  },
+  {
+    id: 20,
+
+    pand: "house",
+
+    prijs: 380.0,
+
+    oppervlak: 310,
+
+    stad: "Antwerpen",
+
+    kamers: 3,
+
+    tuin: true,
+  },
+];
